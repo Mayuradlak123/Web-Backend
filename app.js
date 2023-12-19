4c2fb3bffb7dea0adb82cde8542859f7898d9e25
Mayur Adlak <136291083+mayuradlakbbt@users.noreply.github.com>
2023-12-19 17:53:32 +0530

    Changed Config

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const serviceRoutes = require("./api/routes/contacts.routes")
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.use(bodyParser.json());
 app.use("/services", serviceRoutes);
