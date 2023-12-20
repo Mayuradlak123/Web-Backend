58a296a698fa21cd8e0b83a1003de7601ecd0fb6
Mayur Adlak <136291083+mayuradlakbbt@users.noreply.github.com>
2023-12-20 22:00:37 +0530

    Deploye

--- app.js
+++ app.js
@@ -14,6 +14,21 @@ app.use("/services", serviceRoutes);
 app.get("/", function(req, res) {
   res.json({message:"App Launched successfully"})
 })
+var whitelist = ['http://localhost:5173',"http://127.0.0.1:5173/", "http://localhost:3306"]
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true);
+            if (whitelist.indexOf(origin) === -1) {
+                const msg =
+                    "The CORS policy for this site does not " +
+                    "allow access from the specified Origin.";
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        },
+    })
+);
 db()
 app.listen(port);
 // module.exports= app;
